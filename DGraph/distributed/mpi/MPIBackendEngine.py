095f6326ed765a2c11c72563e41631868217d856
Shehtab <szaman5@binghamton.edu>
2024-10-22 16:45:51 -0400

    Setting up static MPI backend variables and methods - Enables allocating MPI windowed GPU memory

--- DGraph/distributed/mpi/MPIBackendEngine.py
+++ DGraph/distributed/mpi/MPIBackendEngine.py
@@ -19,22 +19,58 @@ from torch.utils.dlpack import to_dlpack  # type: ignore
 from torch.utils.dlpack import from_dlpack
 import cupy as cp
 import warnings
+from torch.autograd import Function
+
+
+class MPIGatherFunction(Function):
+    @staticmethod
+    def forward(
+        ctx,
+        send_tensor: torch.Tensor,
+        indices: torch.LongTensor,
+    ):
+        ctx.save_for_backward(send_tensor, indices)
+        bs = send_tensor.shape[0]
+        send_size = send_tensor.shape[2]
+        num_indices = indices.shape[1]
+
+        recv_tensor = MPIBackendEngine.Malloc(bs * num_indices * send_size).reshape(
+            bs, num_indices, send_size
+        )
+        return recv_tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        send_tensor, indices = ctx.saved_tensors
+        size = send_tensor.numel()
+        bs = send_tensor.shape[0]
+        num_nodes = send_tensor.shape[1]
+        feat_size = send_tensor.shape[2]
+        grad_input = MPIBackendEngine.Malloc(size).reshape(bs, num_nodes, feat_size)
+        return grad_input, None
 
 
 class MPIBackendEngine(BackendEngine):
+    _is_initialized = False
+    _rank = -1
+    _world_size = -1
+
     def __init__(self, *args, **kwargs):
-        self._iniitalized = dist.is_initialized()
+        # self._iniitalized = dist.is_initialized()
+        pass
 
     def init_process_group(self, *args, **kwargs):
-        if not self._initialized:
+        if not MPIBackendEngine._is_initialized:
             # We want both NCCL and MPI to be initialized
             dist.init_process_group(backend="nccl", *args, **kwargs)
             # Dist initialization is done by the user and handles
             # the collective operations need for SGD
-
             MPI.Init()
-            self._initialized = True
+            MPIBackendEngine._is_initialized = True
+
             self._comm = MPI.COMM_WORLD
+            MPIBackendEngine._rank = self._comm.Get_rank()
+            MPIBackendEngine._world_size = self._comm.Get_size()
 
     def finalize(self):
         if self._initialized:
@@ -42,13 +78,24 @@ class MPIBackendEngine(BackendEngine):
 
             self._initialized = False
 
-    def Malloc(self, size: int) -> torch.Tensor:
+    @staticmethod
+    def Malloc(size: int) -> torch.Tensor:
         """Allocates memory on the GPU that is accessible by MPI one-sided communication"""
 
         cupy_tensor = cp.empty(size, dtype=cp.float32)
         MPI.Win.Attach(cupy_tensor.data.ptr)  # type: ignore
         torch_tensor = from_dlpack(to_dlpack(cupy_tensor))  # Zero copy
         return torch_tensor
+
+    def get_local_rank_slice(self, tensor: torch.Tensor) -> torch.Tensor:
+        rank = self.get_rank()
+        world_size = self.get_world_size()
+        tensor_shape = tensor.shape
+        tensor_size = tensor_shape[1]
+        local_size = tensor_size // world_size
+        start_index = rank * local_size
+        end_index = start_index + local_size
+        return tensor[:, start_index:end_index]
 
     def get_rank(self) -> int:
         return self._comm.Get_rank()
@@ -73,7 +120,6 @@ class MPIBackendEngine(BackendEngine):
                 "Scatter indices not on CPU, moving to CPU."
                 + "MPI requires indices to be on CPU."
             )
-        raise NotImplementedError
 
     def gather(self, *args, **kwargs) -> torch.Tensor:
         input_tensor: torch.Tensor = args[0]
@@ -92,5 +138,3 @@ class MPIBackendEngine(BackendEngine):
                 "Gather indices not on CPU, moving to CPU."
                 + "MPI requires indices to be on CPU."
             )
-
-        raise NotImplementedError
--- tests/test_mpi_backend.py
+++ tests/test_mpi_backend.py
@@ -11,6 +11,22 @@ def init_mpi_backend():
     return comm
 
 
+@pytest.fixture(scope="module")
+def setup_gather_data():
+    all_rank_input_data = torch.randn(1, 4, 64)
+
+    all_edge_coo = torch.tensor([[0, 0, 0, 1, 1, 2, 2, 3], [1, 2, 3, 0, 3, 0, 3, 0]])
+    rank_mappings = torch.tensor([[0, 0, 0, 0, 0, 1, 1, 1], [0, 1, 1, 0, 1, 0, 1, 0]])
+
+    all_rank_output = torch.zeros(2, 8, 64)
+
+    for k in range(2):
+        for i in range(8):
+            all_rank_output[k][i] = all_rank_input_data[:, all_edge_coo[k, i]]
+
+    return all_rank_input_data, all_edge_coo, rank_mappings, all_rank_output
+
+
 def test_mpi_backend_init(init_mpi_backend):
     comm = init_mpi_backend
     rank = comm.get_rank()
