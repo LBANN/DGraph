a82e5308c73ea3f03ca6e831adbf4fe42f0f0c30
Shehtab Zaman <szaman5@binghamton.edu>
2024-10-28 22:06:54 -0700

    Added kernel launch integration with Torch tensors - Also adds additional symmetric heap memory allocator

--- DGraph/distributed/csrc/torch_nvshmem_p2p.cu
+++ DGraph/distributed/csrc/torch_nvshmem_p2p.cu
@@ -18,6 +18,8 @@
 #include "mpi.h"
 #include "macros.hpp"
 #include "nvshmem.h"
+#include "nvshmem_comm_kernels.cuh"
+#include <functional>
 
 // Don't know if making these statc is the right thing to do
 // but going with it for now, will need to revisit - S.Z
@@ -26,7 +28,8 @@ bool NVSHMEMP2P::m_initialized = false;
 int NVSHMEMP2P::m_rank = 0;
 int NVSHMEMP2P::m_world_size = 0;
 
-void NVSHMEMP2P::init(int rank, int world_size) {
+void NVSHMEMP2P::init(int rank, int world_size)
+{
   MPI_Comm mpi_comm = MPI_COMM_WORLD;
   nvshmemx_init_attr_t attr;
   MPI_Init(NULL, NULL);
@@ -45,8 +48,10 @@ void NVSHMEMP2P::init(int rank, int world_size) {
   m_initialized = true;
 }
 
-void NVSHMEMP2P::finalize() {
-  if (!m_initialized) {
+void NVSHMEMP2P::finalize()
+{
+  if (!m_initialized)
+  {
     throw std::runtime_error("NVSHMEMP2P is not initialized");
   }
   nvshmem_finalize();
@@ -54,29 +59,118 @@ void NVSHMEMP2P::finalize() {
   m_initialized = false;
 }
 
-void NVSHMEMP2P::dist_put(torch::Tensor src, torch::Tensor dst, torch::Tensor indices) {
-  CHECK_INPUT(src);
-  CHECK_INPUT(dst);
+void NVSHMEMP2P::dist_put(torch::Tensor input,
+                          torch::Tensor output,
+                          torch::Tensor indices,
+                          const int mini_batches,
+                          const int num_input_rows,
+                          const int num_cols,
+                          const int num_output_rows)
+{
+  CHECK_INPUT(input);
+  CHECK_INPUT(output);
   CHECK_INPUT(indices);
-  if (!m_initialized) {
+
+  TORCH_CHECK(input.is_contiguous());
+  TORCH_CHECK(output.is_contiguous());
+  TORCH_CHECK(indices.is_contiguous());
+  TORCH_INTERNAL_ASSERT(input.device().type() == at::DeviceType::CUDA);
+  TORCH_INTERNAL_ASSERT(output.device().type() == at::DeviceType::CUDA);
+  TORCH_INTERNAL_ASSERT(indices.device().type() == at::DeviceType::CUDA);
+
+  if (!m_initialized)
+  {
     throw std::runtime_error("NVSHMEMP2P is not initialized");
   }
+
+  // Get the pointers to the data
+  const float *input_ptr = input.data_ptr<float>();
+  const long *indices_ptr = indices.data_ptr<long>();
+  float *output_ptr = output.data_ptr<float>();
+
+  dim3 block_dims, grid_dims;
+  block_dims.x = 1;
+  block_dims.y = 16;
+  block_dims.z = 1;
+
+  grid_dims.z = (mini_batches + block_dims.x - 1) / block_dims.z;
+  grid_dims.y = (num_output_rows + block_dims.y - 1) / block_dims.y;
+  grid_dims.x = (num_cols + block_dims.x - 1) / block_dims.x;
+
+  // Launch the kernel
+  NVSHMEM::Scatter_NVSHMEM_Kernel<<<grid_dims, block_dims>>>(input_ptr,
+                                                             indices_ptr,
+                                                             output_ptr,
+                                                             mini_batches,
+                                                             num_input_rows,
+                                                             num_cols,
+                                                             num_output_rows);
 }
 
-void NVSHMEMP2P::dist_get(torch::Tensor src, torch::Tensor dst, torch::Tensor indices) {
-  CHECK_INPUT(src);
-  CHECK_INPUT(dst);
+void NVSHMEMP2P::dist_get(torch::Tensor input,
+                          torch::Tensor output,
+                          torch::Tensor indices,
+                          const int mini_batches,
+                          const int num_input_rows,
+                          const int num_cols,
+                          const int num_output_rows)
+{
+  CHECK_INPUT(input);
+  CHECK_INPUT(output);
   CHECK_INPUT(indices);
-  if (!m_initialized) {
+  TORCH_CHECK(input.is_contiguous());
+  TORCH_CHECK(output.is_contiguous());
+  TORCH_CHECK(indices.is_contiguous());
+  TORCH_INTERNAL_ASSERT(input.device().type() == at::DeviceType::CUDA);
+  TORCH_INTERNAL_ASSERT(output.device().type() == at::DeviceType::CUDA);
+  TORCH_INTERNAL_ASSERT(indices.device().type() == at::DeviceType::CUDA);
+  if (!m_initialized)
+  {
     throw std::runtime_error("NVSHMEMP2P is not initialized");
   }
+
+  // Get the pointers to the data
+  const float *input_ptr = input.data_ptr<float>();
+  const long *indices_ptr = indices.data_ptr<long>();
+  float *output_ptr = output.data_ptr<float>();
+  dim3 block_dims, grid_dims;
+
+  block_dims.x = 1;
+  block_dims.y = 16;
+  block_dims.z = 1;
+
+  grid_dims.z = (mini_batches + block_dims.x - 1) / block_dims.z;
+  grid_dims.y = (num_output_rows + block_dims.y - 1) / block_dims.y;
+  grid_dims.x = (num_cols + block_dims.x - 1) / block_dims.x;
+  // Launch the kernel
+  NVSHMEM::Gather_NVSHMEM_Kernel_Warp<<<grid_dims, block_dims>>>(input_ptr,
+                                                                 indices_ptr,
+                                                                 output_ptr,
+                                                                 mini_batches,
+                                                                 num_input_rows,
+                                                                 num_cols,
+                                                                 num_output_rows);
 }
 
-// PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-//   py::class_<NVSHMEMP2P>(m, "NVSHMEMP2P")
-//     .def(py::init<>())
-//     .def("init", &NVSHMEMP2P::init)
-//     .def("finalize", &NVSHMEMP2P::finalize)
-//     .def("dist_put", &NVSHMEMP2P::dist_put)
-//     .def("dist_get", &NVSHMEMP2P::dist_get);
-// }
+torch::Tensor NVSHMEMP2P::AllocateSymmetricMemory(const int size)
+{
+  if (size <= 0)
+  {
+    throw std::runtime_error("Invalid size");
+  }
+
+  if (!m_initialized)
+  {
+    throw std::runtime_error("NVSHMEMP2P is not initialized");
+  }
+
+  void *ptr = nvshmem_malloc((size_t)size);
+
+  std::function<void(void *)> deleter = [](void *ptr)
+  {
+    nvshmem_free(ptr);
+  };
+  // See torch::from_blob for more details
+  // https://pytorch.org/cppdocs/api/function_namespacetorch_1ad7fb2a7759ef8c9443b489ddde494787.html
+  return torch::from_blob(ptr, {size}, {1}, deleter);
+}
--- DGraph/distributed/nvshmem/NVSHMEMBackendEngine.py
+++ DGraph/distributed/nvshmem/NVSHMEMBackendEngine.py
@@ -11,21 +11,78 @@
 # https://github.com/LBANN and https://github.com/LLNL/LBANN.
 #
 # SPDX-License-Identifier: (Apache-2.0)
+from torch.utils.dlpack import from_dlpack
 import torch
 import torch.distributed as dist
 from DGraph.distributed.Engine import BackendEngine
 import torch_nvshmem_p2p as nvshmem
+import cupy as cp
+import warnings
+from torch.autograd import Function
 
+def _nvshmmem_gather(send_tensor, indices, gathered_tensor):
+    # Gather the tensors
+    gathered_tensors = [torch.zeros_like(send_tensor) for _ in range(nvshmem.get_world_size())]
+    gathered_tensors[nvshmem.get_rank()] = send_tensor
+    dist.all_gather(gathered_tensors, send_tensor)
+
+    # Gather the indices
+    gathered_indices = [torch.zeros_like(indices) for _ in range(nvshmem.get_world_size())]
+    gathered_indices[nvshmem.get_rank()] = indices
+    dist.all_gather(gathered_indices, indices)
+
+    return gathered_tensors
+
+def _nvshmem_scatter(input_tensor, indices, scattered_tensor):
+    # Scatter the tensors
+    scattered_tensors = [torch.zeros_like(input_tensor) for _ in range(nvshmem.get_world_size())]
+    scattered_tensors[nvshmem.get_rank()] = input_tensor
+    dist.all_gather(scattered_tensors, input_tensor)
+
+    # Scatter the indices
+    scattered_indices = [torch.zeros_like(indices) for _ in range(nvshmem.get_world_size())]
+    scattered_indices[nvshmem.get_rank()] = indices
+    dist.all_gather(scattered_indices, indices)
+
+    return scattered_tensors
+
+
+class NVSHMEMGatherFunction(Function):
+    @staticmethod
+    def forward(ctx, send_tensor, indices, gathered_tensor):
+        gathered_tensors = _nvshmmem_gather(send_tensor, indices, gathered_tensor)
+        return gathered_tensors
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output, None, None
+    
+class NVSHMEMScatterFunction(Function):
+    @staticmethod
+    def forward(ctx, input_tensor, indices, scattered_tensor):
+        scattered_tensors = _nvshmem_scatter(input_tensor, indices, scattered_tensor)
+        return scattered_tensors
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output, None, None
 
 class NVSHMEMBackendEngine(BackendEngine):
+    _is_initialized = False
+    _rank = -1
+    _world_size = -1
+    _ranks_per_graph = -1
+    
     def __init__(self, *args, **kwargs):
         # check if already initialized
         self._initialized = dist.is_initialized()
 
     def init_process_group(self, *args, **kwargs):
         if not self._initialized:
-            dist.init_process_group(backend="nccl", *args, **kwargs)
             nvshmem.init()
+
+            dist.init_process_group(backend="nccl", *args, **kwargs)
+            
             self._initialized = True
 
     def get_rank(self) -> int:
