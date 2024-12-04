ad86d0eb9abbc36db6b430a0cf9605b95091aa2e
Brian C. Van Essen <vanessen1@llnl.gov>
2024-12-04 09:25:10 -0800

    Set a minimum version of mpi4py to avoid known bugs.

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     name="DGraph",
     py_modules=["DGraph"],
     # ext_modules=[nvshmem_module],
-    install_requires=["torch", "numpy", "ninja", "mpi4py"],
+    install_requires=["torch", "numpy", "ninja", "mpi4py>=3.1.4"],
     cmdclass={"build_ext": BuildExtension},
 )
