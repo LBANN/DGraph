6671a163f881fc8f68fcb4839269f8f042b80a5d
Tom Benson <benson31@llnl.gov>
2025-04-24 13:32:42 -0700

    Updates to FindNVSHMEM module

--- cmake/FindNVSHMEM.cmake
+++ cmake/FindNVSHMEM.cmake
@@ -1,75 +1,65 @@
 ################################################################################
-## Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
-## DiHydrogen Project Developers. See the top-level LICENSE file for details.
+## Copyright (c) 2014-2022, Lawrence Livermore National Security, LLC.
+## Produced at the Lawrence Livermore National Laboratory.
+## Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+## the CONTRIBUTORS file. <lbann-dev@llnl.gov>
 ##
-## SPDX-License-Identifier: Apache-2.0
+## LLNL-CODE-697807.
+## All rights reserved.
+##
+## This file is part of LBANN: Livermore Big Artificial Neural Network
+## Toolkit. For details, see http://software.llnl.gov/LBANN or
+## https://github.com/LLNL/LBANN.
+##
+## Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+## may not use this file except in compliance with the License.  You may
+## obtain a copy of the License at:
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+## implied. See the License for the specific language governing
+## permissions and limitations under the license.
 ################################################################################
 
-# Defines the following variables:
-#   - NVSHMEM_FOUND
-#   - NVSHMEM_LIBRARIES
-#   - NVSHMEM_INCLUDE_DIRS
+# Output variables
 #
-# Also creates an imported target NVSHMEM
-
-# Find the header
-find_path(NVSHMEM_INCLUDE_DIRS nvshmem.h
-  HINTS ${NVSHMEM_DIR} $ENV{NVSHMEM_DIR}
-  PATH_SUFFIXES include
-  NO_DEFAULT_PATH
-  DOC "Directory with NVSHMEM header.")
-find_path(NVSHMEM_INCLUDE_DIRS nvshmemx.h)
+#   NVSHMEM_FOUND
+#   NVSHMEM_LIBRARY
+#   NVSHMEM_INCLUDE_DIR
+#
+# Also creates an imported target NVSHMEM::NVSHMEM
 
 # Find the library
 find_library(NVSHMEM_LIBRARY nvshmem
-  HINTS ${NVSHMEM_DIR} $ENV{NVSHMEM_DIR}
-  PATH_SUFFIXES lib64 lib
+  HINTS ${NVSHMEM_HOME} $ENV{NVSHMEM_HOME}
+  ${NVSHMEM_PREFIX} $ENV{NVSHMEM_PREFIX}
+  PATH_SUFFIXES lib lib64
   NO_DEFAULT_PATH
-  DOC "The NVSHMEM library.")
+  DOC "The location of NVSHMEM library.")
 find_library(NVSHMEM_LIBRARY nvshmem)
 
-# Standard handling of the package arguments
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NVSHMEM
-  DEFAULT_MSG
-  NVSHMEM_LIBRARY NVSHMEM_INCLUDE_DIRS)
+# Find the header
+find_path(NVSHMEM_INCLUDE_DIRS nvshmem.h
+  HINTS ${NVSHMEM_HOME} $ENV{NVSHMEM_HOME}
+  ${NVSHMEM_PREFIX} $ENV{NVSHMEM_PREFIX}
+  PATH_SUFFIXES include
+  NO_DEFAULT_PATH
+  DOC "The location of NVSHMEM headers.")
+find_path(NVSHMEM_INCLUDE_DIRS nvshmemx.h)
 
-# Setup the imported target
+# Handle the find_package arguments
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  NVSHMEM DEFAULT_MSG NVSHMEM_LIBRARY NVSHMEM_INCLUDE_DIRS)
+
+# Build the imported target
 if (NOT TARGET NVSHMEM::NVSHMEM)
   add_library(NVSHMEM::NVSHMEM INTERFACE IMPORTED)
-endif (NOT TARGET NVSHMEM::NVSHMEM)
+endif ()
 
-# Set the include directories for the target
-target_include_directories(NVSHMEM::NVSHMEM
-  INTERFACE
-  ${NVSHMEM_INCLUDE_DIRS})
-
-# Set the link libraries for the target
-target_link_libraries(NVSHMEM::NVSHMEM
-  INTERFACE
-  ${NVSHMEM_LIBRARY})
-
-target_compile_definitions(NVSHMEM::NVSHMEM
-  INTERFACE
-  -DNVSHMEM_TARGET)
-
-# # Workaround for separable compilation with cooperative threading. see
-# # https://stackoverflow.com/questions/53492528/cooperative-groupsthis-grid-causes-any-cuda-api-call-to-return-unknown-erro.
-# # Adding this to INTERFACE_COMPILE_OPTIONS does not seem to solve the
-# # problem.  It seems that CMake does not add necessary options for
-# # device linking when cuda_add_executable/library is NOT used. See
-# # also https://github.com/dealii/dealii/pull/5405
-# target_compile_options(NVSHMEM::NVSHMEM
-#   INTERFACE
-#   $<$<COMPILE_LANGUAGE:CUDA>:-gencode arch=compute_70,code=sm_70>)
-
-#
-# Cleanup
-#
-
-# Set the include directories
-mark_as_advanced(FORCE NVSHMEM_INCLUDE_DIRS)
-
-# Set the libraries
-set(NVSHMEM_LIBRARIES NVSHMEM::NVSHMEM)
-mark_as_advanced(FORCE NVSHMEM_LIBRARY)
+target_link_libraries(NVSHMEM::NVSHMEM INTERFACE "${NVSHMEM_LIBRARY}")
+target_include_directories(NVSHMEM::NVSHMEM INTERFACE "${NVSHMEM_INCLUDE_DIRS}")
+target_compile_definitions(NVSHMEM::NVSHMEM INTERFACE "-DNVSHMEM_TARGET")
